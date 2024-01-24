2c38d016a7b5881e0971827f8bd10dfb967d9d5d
juandpt03 <juandpt03@gmail.com>
2024-01-24 15:25:55 -0500

    button clean chat and info button added

--- lib/features/home/presentation/screens/home/views/chat_view.dart
+++ lib/features/home/presentation/screens/home/views/chat_view.dart
@@ -28,12 +28,88 @@ class ChatView extends StatelessWidget {
             Text('¡Tú chat de confianza!', style: subTextStyle),
           ],
         ),
+        actions: const [
+          _IconInfo(),
+          _IconClearchat(),
+        ],
         elevation: 1,
       ),
       backgroundColor: colors.surface,
       body: Center(
         child: _ChatView(),
       ),
+    );
+  }
+}
+
+class _IconClearchat extends ConsumerWidget {
+  const _IconClearchat();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bool isEmpty = ref.watch(messageProvider).isEmpty;
+
+    return IconButton(
+      icon: const Icon(Icons.delete_outline),
+      onPressed: isEmpty
+          ? null
+          : () {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text('¿Estás seguro?'),
+                  content: const Text(
+                      '¿Estás seguro de que quieres borrar el chat?'),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Cancelar'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        ref.invalidate(messageProvider);
+
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Aceptar'),
+                    ),
+                  ],
+                ),
+              );
+            },
+    );
+  }
+}
+
+class _IconInfo extends StatelessWidget {
+  const _IconInfo();
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.info_outline),
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Información'),
+              content: const Text(
+                  'ChatDPT es un chatbot que te ayudará a resolver cualquiera de tus dudas\n\n©Juandpt '),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Aceptar'),
+                ),
+              ],
+            );
+          },
+        );
+      },
     );
   }
 }
